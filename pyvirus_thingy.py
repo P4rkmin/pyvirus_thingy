c292fa0b65d7e7b79b07eee407c006db96d27db6
Parker <parkerblackman.pb@gmail.com>
2024-07-11 16:02:08 -0500

    Add files via upload

---
+++ pyvirus_thingy.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QVBoxLayout
+
+class PopupApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Popup Example')
+
+        layout = QVBoxLayout()
+
+        self.button = QPushButton('Press me', self)
+        self.button.clicked.connect(self.show_popup)
+
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def show_popup(self):
+        QMessageBox.information(self, 'Popup', 'Button Pressed!')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = PopupApp()
+    ex.show()
+    sys.exit(app.exec_())
